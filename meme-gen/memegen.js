26ac97008c9f0b2539c57de0b0cd76d116888e18
Kevin Nguyen <kayvinhnguyen@gmail.com>
2023-01-31 17:04:58 -0800

    added event listeners

---
+++ meme-gen/memegen.js
@@ -0,0 +1,34 @@
+const btn = document.querySelector('.add-button');
+const memeSection = document.querySelector('.meme-container');
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    // grab values inside form inputs
+    const imgUrl = document.querySelector('#img-url').value;
+    const topText = document.querySelector('#top-text').value;
+    const botText = document.querySelector('#bot-text').value;
+
+    const meme = document.createElement('div');
+    meme.classList.add('meme-item');
+
+    const memeImg = document.createElement('img');
+    memeImg.src = imgUrl;
+
+    const topCaption = document.createElement('div')
+    topCaption.classList.add('meme-item-top')
+    topCaption.innerText = topText;
+
+    const botCaption = document.createElement('div')
+    botCaption.classList.add('meme-item-bot')
+    botCaption.innerText = botText;
+
+    memeSection.appendChild(meme)
+    meme.appendChild(memeImg)
+    meme.appendChild(topCaption)
+    meme.appendChild(botCaption)
+
+    memeImg.addEventListener('click', (e) => {
+        memeImg.parentNode.remove();
+    });
+})
